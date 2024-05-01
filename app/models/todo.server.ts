1c04c3f66bb34495a5e646d570e51a02f695fdda
Yusuke Nakamura <yaneuranoanmaku@yahoo.co.jp>
2024-05-02 00:41:18 +0900

    add todo.server.ts

---
+++ app/models/todo.server.ts
@@ -0,0 +1,28 @@
+import { prisma } from "~/db.server";
+import { Todo } from "@prisma/client";
+
+export const getTodos = () => {
+  return prisma.todo.findMany();
+};
+
+export const getTodo = (id: string) => {
+  return prisma.todo.findUnique({ where: { id } });
+};
+
+export const createTodo = (todo: Pick<Todo, "title" | "deadline">) => {
+  return prisma.todo.create({ data: todo });
+};
+
+export const updateTodo = (
+  id: string,
+  todo: Pick<Todo, "title" | "deadline" | "isDone">,
+) => {
+  return prisma.todo.update({
+    where: { id },
+    data: todo,
+  });
+};
+
+export const deleteTodo = (id: string) => {
+  return prisma.todo.delete({ where: { id } });
+};
